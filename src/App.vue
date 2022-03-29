7ef454b58f8c051fdf4bd88e2d2d7c8c5d78007e
dymafr <erwan1309@hotmail.com>
2022-03-29 12:54:59 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="p-20">
+    <button class="btn btn-primary mb-10" @click="show = !show">
+      Cliquez !
+    </button>
+    <div id="container">
+      <Transition
+        @appear="appearEnter"
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @leave="leave"
+      >
+        <p v-show="show" id="balle"></p>
+      </Transition>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const show = ref(true);
+
+function appearEnter(el,done) => {
+  let opacity = 0;
+  const intervalId = setInterval(() => {
+    if (opacity >= 1) {
+        clearInterval(intervalId);
+        done();
+    }
+    el.style.opacity = opacity;
+    opacity += 0.1;
+  }, 100);
+}
+
+    function beforeEnter(el) => {
+      el.style.opacity = 1;
+      el.style.top = 0;
+      el.style.left = 0;
+    }
+    function enter(el, done) => {
+      let pos = 0;
+      let dir = 'down';
+      let intervalId = setInterval(() => {
+        if (pos < 0 ) {
+            clearInterval(intervalId);
+            done();
+        } else if (dir === 'up') {
+            pos--;
+            el.style.top = pos + 'px';
+        } else if (pos === 280) {
+            dir = 'up';
+        } else {
+            pos++;
+            el.style.top = pos + 'px';
+        }
+      }, 5)
+    },
+    function leave(el, done) => {
+      let opacity = 1;
+      const intervalId = setInterval(() => {
+        if (opacity <= 0.1) {
+            clearInterval(intervalId);
+            done();
+        }
+        el.style.opacity = opacity;
+        opacity -= opacity *  0.1;
+      }, 100);
+    }
+</script>
+
+<style lang="scss">
+@import './assets/scss/base.scss';
+#container {
+  width: 500px;
+  height: 500px;
+  position: relative;
+  background: black;
+}
+#balle {
+  position: absolute;
+  width: 200px;
+  height: 200px;
+  margin-left: 150px;
+  border-radius: 55%;
+  background-image: -webkit-radial-gradient(
+    65px 65px,
+    farthest-side,
+    #eeeeee 0%,
+    #53cbf1 40%,
+    #002245 100%
+  );
+}
+</style>
